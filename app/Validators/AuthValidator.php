a1be5c70e1134d7bf9c73a5f7e8634cda974bdb9
Nando Agustin Bravo <bravonandoagustin@gmail.com>
2024-01-12 12:26:19 -0300

    refactor(auth): :safety_vest: Add the validation for the confirmation of the password

--- app/Validators/AuthValidator.php
+++ app/Validators/AuthValidator.php
@@ -15,20 +15,22 @@ class AuthValidator implements AuthValidatorInterface
         $rules = [
             'name' => ['required', 'max:60', 'string', new ForbiddenWordsRule],
             'email' => ['required', 'max:100', 'email'],
-            'password' => ['required', 'min:8', 'max:255', 'regex:/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/']
-
+            'password' => ['required', 'min:8', 'max:255', 'regex:/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/'],
+            'confirmation_password' => ['required','same:password']
         ];
 
         $messages = [
             'name.required' => 'The name is required, please enter a name to continue',
             'name.max' => 'The name must have less than 60 characters.',
             'name.string' => 'The name must be a text.',
-            'email.required' => 'The email is required. please enter a email to continue',
+            'email.required' => 'The email is required. please enter a email to continue.',
             'email.max' => 'The email must have less than 100 characters.',
             'email.email' => 'The email must be in a valid email format.',
-            'password.required' => 'The password is required, please enter a name to continue',
+            'password.required' => 'The password is required, please enter a name to continue.',
             'password.max' => 'The password must have less than 100 characters.',
-            'password.regex' => 'The password must have at least one letter capital, a number and a symbol.'
+            'password.regex' => 'The password must have at least one letter capital, a number and a symbol.',
+            'confirmation_password.required' => 'The confirmation of the password is required.',
+            'confirmation_password.same' => 'The confirmation of the password has to be equal to the written password.',
         ];
 
         // Accedemos a la clase Validator en el espacio de nombres global, no es Illuminate\Contracts\Validation\Validator
